bd8669f53797dd53d161235046a736ce608798a9
ljjehl <ljjehl@gmail.com>
2018-01-08 13:28:20 +0100

    clean up some ril

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -11,16 +11,8 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.carrier.rc
 LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
+LOCAL_MODULE_CLASS      := BOOT
 LOCAL_SRC_FILES         := init.carrier.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.rilcommon.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := init.rilcommon.rc
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -62,14 +54,6 @@ LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
 LOCAL_SRC_FILES		:= init.wifi.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.samsung.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := init.samsung.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
